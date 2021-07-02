da07a6ad731807c393a331aa1b1420fd06042dac
vimukthi <vimukthi@efuturesworld.com>
2021-07-02 23:22:43 +0530

    create static variables update .gitignore

--- btre/settings.py
+++ btre/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # create constant for static content path
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
+# we need to create STATICFILES_DIRS because collectstatic command use this to locate static contents
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
