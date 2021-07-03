53f99b81ab00fdffe40aa2c70d1f9ff05615dad0
vimukthi <vimukthi@efuturesworld.com>
2021-07-03 21:57:08 +0530

    set image links and urls

--- btre/urls.py
+++ btre/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('pages.urls')),  # works like prefix for pages URLs
-    path('listings/', include('listings.urls')),
-    path('admin/', admin.site.urls),
-]
+                  path('', include('pages.urls')),  # works like prefix for pages URLs
+                  path('listings/', include('listings.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # add media related URLS
