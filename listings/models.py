31ccf1c8270b5c4122c76ef3d4cb4d4e2f22bd49
vimukthi <vimukthi@efuturesworld.com>
2021-07-03 20:50:43 +0530

    create migrations and models for Listing and Realtor apps

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import datetime
+from realtors.models import Realtor
 
-# Create your models here.
+
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d',
+                                   blank=True)  # we create folder for each date to upload images
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.datetime.now(), blank=True)
+
+    """
+    The __str__ method is useful for a string representation of the object, 
+    either when someone codes in str(your_object), or even when someone might do print(your_object).
+    The __str__ method is one that should be the most human-readable possible, 
+    yet also descriptive of that exact object.
+    """
+
+    def __str__(self):
+        return self.title
