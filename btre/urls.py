d0d26a000d652275689d3a4c0084fe596e01db3a
vimukthi <vimukthi@efuturesworld.com>
2021-07-03 17:49:34 +0530

    add update views,links in listings

--- btre/urls.py
+++ btre/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),  # works like prefix for pages URLs
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ listings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='listings'),  # /listings
+    # we add prefix for every app. here it would be listings . so complete URL is /listings
+    path('<int:listing_id>', views.listing, name='listing'),  # /listings/?listing_id=12
+    path('search', views.search, name='search')  # listings/search
+]
