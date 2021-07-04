970dea4d1a02edc85292c3f792869a3ebb73844f
vimukthi <vimukthi@efuturesworld.com>
2021-07-04 17:29:04 +0530

    load data fro db and show in frontend

--- listings/urls.py
+++ listings/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='listings'),  # /listings
     # we add prefix for every app. here it would be listings . so complete URL is /listings
-    path('<int:listing_id>', views.listing, name='listing'),  # /listings/?listing_id=12
+    path('<int:listing_id>', views.listing, name='listing'),  # /listings/12
     path('search', views.search, name='search')  # listings/search
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .models import Listing
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    data = {
+        'listings': listings,
+        'id': 5
+    }
+    # return HttpResponse((len(listings)))
+    return render(request, 'listings/listings.html', data)
 
 
-def listing(request):
+def listing(request, listing_id):  # passed Id from route
     return render(request, 'listings/listing.html')
 
 
